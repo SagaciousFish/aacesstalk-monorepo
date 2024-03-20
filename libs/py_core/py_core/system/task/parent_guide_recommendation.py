ec9a31965c0117cbdeac8d90af2a96127090b01b
Young-Ho Kim <yghokim@younghokim.net>
2024-03-20 15:46:55 +0900

    Revised ParentGuideRecommendation to be generated in English under the hood.

--- libs/py_core/py_core/system/task/parent_guide_recommendation.py
+++ libs/py_core/py_core/system/task/parent_guide_recommendation.py
@@ -2,9 +2,11 @@ from typing import TypeAlias
 
 from chatlib.llm.integration.openai_api import GPTChatCompletionAPI, ChatGPTModel
 from chatlib.tool.converter import generate_type_converter
-from chatlib.tool.versatile_mapper import ChatCompletionFewShotMapper, ChatCompletionFewShotMapperParams
+from chatlib.tool.versatile_mapper import ChatCompletionFewShotMapper, ChatCompletionFewShotMapperParams, \
+    MapperInputOutputPair
 
-from py_core.system.model import ParentGuideRecommendationResult, Dialogue, ParentGuideElement
+from py_core.system.model import ParentGuideRecommendationResult, Dialogue, ParentGuideElement, DialogueMessage, \
+    DialogueRole, CardInfo, CardCategory
 from py_core.system.task.stringify import convert_dialogue_to_str
 
 ParentGuideRecommendationAPIResult: TypeAlias = list[ParentGuideElement]
@@ -14,6 +16,30 @@ str_output_converter, output_str_converter = generate_type_converter(ParentGuide
 
 class ParentGuideRecommendationParams(ChatCompletionFewShotMapperParams):
     pass
+
+
+PARENT_GUIDE_EXAMPLES: list[MapperInputOutputPair[Dialogue, ParentGuideRecommendationAPIResult]] = [
+    MapperInputOutputPair(
+        input=[
+            DialogueMessage(role=DialogueRole.Parent, content="오늘 학교에서 뭐 했어?"),
+            DialogueMessage(role=DialogueRole.Child, content=[
+                CardInfo(text="Friend", category=CardCategory.Noun, recommendation_id=""),
+                CardInfo(text="Lunch", category=CardCategory.Noun, recommendation_id=""),
+                CardInfo(text="Delicious", category=CardCategory.Emotion, recommendation_id=""),
+            ]),
+        ],
+        output=[
+            ParentGuideElement(
+                example="Glad to hear that you've had delicious lunch! Whom did you eat with?",
+                guide="Acknowledge what your child said and ask detailed information."
+            ),
+            ParentGuideElement(
+                example="What did you eat? Which food was delicious?",
+                guide="Ask follow-ups to know your kid better. Then you will be able to continue the conversation based on the food that the kid liked."
+            )
+        ]
+    )
+]
 
 
 class ParentGuideRecommendationGenerator:
@@ -36,16 +62,16 @@ Given the last message of the children, suggest a list of sentences that can hel
 
 Return a JSON object in the following format:
 Array<{
-        "example": Provide response or question that parents can make to the child's message.
+        "example": Provide response or question that parents can make to the child's message in English.
         "guide": Provide guide for parents of autistic children to consider when responding to the child's answer. Keep the guide short and concise so parents can read it quickly and move on.
     }>
 
-The result should be in Korean and please provide up to three options for each element.
+Please provide up to three options.
 """
         return prompt
 
     async def generate(self, dialogue: Dialogue) -> ParentGuideRecommendationResult:
-        guide_list: ParentGuideRecommendationAPIResult = await self.__mapper.run(None, dialogue,
+        guide_list: ParentGuideRecommendationAPIResult = await self.__mapper.run(PARENT_GUIDE_EXAMPLES, dialogue,
                                                                                  ParentGuideRecommendationParams(
                                                                                      model=ChatGPTModel.GPT_4_0613,
                                                                                      api_params={}))
