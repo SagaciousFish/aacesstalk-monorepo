26c9ceb7f0e38cf1734ab06917e79e1216d23a59
Young-Ho Kim <yghokim@gmail.com>
2024-03-30 00:44:16 +0900

    Print deepl translation elapsed time.

--- libs/py_database/test_cli.py
+++ libs/py_database/test_cli.py
@@ -4,12 +4,14 @@ from chatlib.global_config import GlobalConfig
 from chatlib.llm.integration.openai_api import GPTChatCompletionAPI
 from py_core import ModeratorSession
 from py_core.cli import test_session_loop
+from py_core.utils.deepl_translator import DeepLTranslator
 from py_database import SQLSessionStorage
 from py_database.database import create_database_engine, get_async_session, create_db_and_tables
 
 
 GlobalConfig.is_cli_mode = True
 GPTChatCompletionAPI.assert_authorize()
+DeepLTranslator.assert_authorize()
 
 engine = create_database_engine("database-test.db", verbose=True)
 session = get_async_session(engine)
