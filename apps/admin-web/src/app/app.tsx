e29cfa19ea4f968b7bba1be98f33cf5618f74c57
Young-Ho Kim <yghokim@gmail.com>
2024-07-20 23:32:11 +0900

    Removed unnecessary console logs.

--- apps/admin-web/src/app/app.tsx
+++ apps/admin-web/src/app/app.tsx
@@ -11,7 +11,6 @@ import moment from 'moment-timezone';
 export function App() {
 
     useEffect(()=>{
-        console.log(import.meta.env.VITE_BACKEND_ADDRESS)
         Http.initialize(import.meta.env.VITE_BACKEND_ADDRESS, async () => { return moment.tz.guess(true) })
     },[])
 
