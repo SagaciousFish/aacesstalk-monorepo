321440fb0074109a6314474aff8cc8d40215114a
Young-Ho Kim <yghokim@gmail.com>
2024-12-27 23:37:05 +0900

    Fixed className override issues using tailwind-merge.

--- apps/client-rn/src/components/tailwind-components.tsx
+++ apps/client-rn/src/components/tailwind-components.tsx
@@ -1,6 +1,7 @@
 import { ButtonProps, Pressable, View, Text, ViewStyle, PressableAndroidRippleConfig, PressableProps } from "react-native";
 import { styleTemplates } from "../styles";
 import { useMemo } from "react";
+import { twMerge } from 'tailwind-merge'
 
 export const TailwindButton = (props: {
     containerClassName?: string
@@ -19,11 +20,23 @@ export const TailwindButton = (props: {
         return {color: props.rippleColor || "##f9aa3330"}
     }, [props.rippleColor])
 
-    return <View accessible={false} className={`overflow-hidden ${props.shadowClassName || "shadow-lg shadow-slate-500/50"} ${props.roundedClassName} ${props.containerClassName}`} removeClippedSubviews={true}>
+    const containerClassName = useMemo(()=>{
+        return twMerge('overflow-hidden', props.shadowClassName || "shadow-lg shadow-slate-500/50", props.roundedClassName, props.containerClassName)
+    }, [props.shadowClassName, props.roundedClassName, props.containerClassName])
+
+    const buttonClassName = useMemo(()=>{
+        return twMerge('items-center flex-row justify-center px-8 py-3 bg-white', props.buttonStyleClassName, props.roundedClassName, props.disabled === true ? (props.disabledButtonStyleClassName || "") : "")
+    }, [props.buttonStyleClassName, props.roundedClassName, props.disabled, props.disabledButtonStyleClassName])
+
+    const titleClassName = useMemo(()=>{
+        return twMerge('text-lg text-center text-slate-600', props.titleClassName, props.disabled === true ? (props.disabledTitleClassName || "") : "")
+    }, [props.titleClassName, props.disabled, props.disabledTitleClassName])
+
+    return <View accessible={false} className={containerClassName} removeClippedSubviews={true}>
         <Pressable accessible={false} aria-selected={false} disabled={props.disabled} android_ripple={rippleConfig} onPress={props.onPress} onLongPress={props.onLongPress}
-        className={`items-center flex-row justify-center px-8 py-3 bg-white ${props.buttonStyleClassName} ${props.roundedClassName} ${props.disabled === true ? (props.disabledButtonStyleClassName || "") : ""}`}>
+        className={buttonClassName}>
             {
-                props.children || <Text className={`text-lg text-center text-slate-600 ${props.titleClassName || ""} ${props.disabled === true ? (props.disabledTitleClassName || "") : ""}`} style={styleTemplates.withBoldFont}>{props.title}</Text>
+                props.children || <Text className={titleClassName} style={styleTemplates.withBoldFont}>{props.title}</Text>
             }
         </Pressable></View>
 }
